272fdd988f30c62d60fd9cd71f2830050f3a238b
franc <onyishif@gmail.com>
2020-11-03 20:03:29 +0100

    add the jobs component delete unnecessary files

---
+++ client/src/Jobs.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import {Grid} from "@material-ui/core"
+
+interface props<T> {
+    jobs: T
+  }
+  
+ const Jobs: React.FC<props<Object>> = ({jobs})=> {
+    return (
+     <div>
+        <div className="dez-bnr-inr dez-bnr-inr-md">
+            <div className="container">
+                <div className="dez-bnr-inr-entry align-m ">
+					<div className="find-job-bx">
+						<p className="site-button button-sm">Find Jobs, Employment & Career Opportunities</p>
+						<h4>Search Based on Personal preferences from <br/> <span className="text-primary">50,000</span> Open Jobs.</h4>
+						<form className="dezPlaceAni">
+							<Grid container spacing={4}>
+								<Grid item className="col-lg-4 col-md-6">
+									<div className="form-group">
+										<label>Job Title, Keywords, or Phrase</label>
+										<div className="input-group">
+											<input type="text" className="form-control" placeholder=""/>
+											<div className="input-group-append">
+											  <span className="input-group-text"><i className="fa fa-search"></i></span>
+											</div>
+										</div>
+									</div>
+								</Grid>
+								<Grid item   alignItems="center" className="col-lg-3 col-md-6">
+									<div className="form-group">
+										<select>
+											<option>All</option>
+											<option>Remote</option>
+										</select>
+									</div>
+								</Grid>
+								<Grid item xs={12} className="col-lg-2 col-md-6">
+									<button type="submit" className="site-button btn-block">Find Job</button>
+								</Grid>
+							</Grid>
+						</form>
+					</div>
+				</div>
+            </div>
+        </div>
+      </div> 
+    );
+  }
+export default Jobs;
