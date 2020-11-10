4833a8c7b847681be40048bd5a05b7cd84306b74
franc <onyishif@gmail.com>
2020-11-10 16:27:56 +0100

    include pagination add count snippets

--- client/src/Jobs.tsx
+++ client/src/Jobs.tsx
@@ -3,11 +3,44 @@ import {Grid} from "@material-ui/core"
 import {JobType} from "./Job"
 import {Job} from "./Job"
 import JobModal from "./JobModal"
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import MobileStepper from '@material-ui/core/MobileStepper';
+import Button from '@material-ui/core/Button';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import Typography from "@material-ui/core/Typography/Typography";
+
+const useStyles = makeStyles({
+	root: {
+	  maxWidth: 400,
+	  flexGrow: 1,
+	  margin: `0 auto`
+	},
+  });
+
 interface props<T> {
     jobs: T[]
   }
  const Jobs: React.FC<props<JobType>> = ({jobs})=> {
   const [open,setOpen] = React.useState(false)
+
+  const classes = useStyles();
+  const theme = useTheme();
+
+  const numJobs = jobs.length;
+  const numOfSteps = Math.ceil(numJobs/50)
+  
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  let jobsList = jobs.slice(activeStep*50,(activeStep*50) + 50)
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
   const starter: JobType = {
 	  title:"",
 	  company:"",
@@ -54,12 +87,15 @@ interface props<T> {
 								</Grid>
 							</Grid>
 						</form>
+						<Typography variant="h6" component="h2">
+							Found {numJobs} Jobs
+						</Typography>
 					</div>
 				</div>
             </div>
         </div>
         {
-          jobs.map((job: JobType,i:number)=>(
+          jobsList.map((job: JobType,i:number)=>(
             <Job key={i} {...job} handleOnClick={e => {
               e.preventDefault();
               setOpen(true)
@@ -67,6 +103,28 @@ interface props<T> {
 			}}>i</Job>
 			))
         }
+		<div className="listCount">
+			Page {activeStep + 1} of {numOfSteps}
+        </div>
+		<MobileStepper
+			variant="progress"
+			steps={numOfSteps}
+			position="static"
+			activeStep={activeStep}
+			className={classes.root}
+			nextButton={
+				<Button size="small" onClick={handleNext} disabled={activeStep === numOfSteps - 1}>
+				Next
+				{theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+				</Button>
+			}
+			backButton={
+				<Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+				{theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+				Back
+				</Button>
+			}
+		/>
       </div> 
     );
   }
